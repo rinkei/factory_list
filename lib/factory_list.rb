32bde4ad589b866337183fddb05a80343f30e9df
rinkei <kei.h.hayashi@gmail.com>
2020-07-22 08:37:43 +0900

    Add FactoryList.list method

--- lib/factory_list.rb
+++ lib/factory_list.rb
@@ -1,6 +1,31 @@
 require "factory_list/version"
+require 'factory_bot'
 
 module FactoryList
   class Error < StandardError; end
-  # Your code goes here...
+
+  def list
+    dirs = ::FactoryBot.definition_file_paths.map(&:to_s)
+    dirs.each do |dir|
+      puts "dir: #{dir}"
+      Dir.glob('**/*.rb', File::FNM_DOTMATCH, base: dir).each do |file|
+        puts "file: #{file}"
+        file_path = File.join(dir, file)
+        File.open(file_path, 'r') do |f|
+          tokens = Ripper.tokenize(f.read)
+          tokens.each_with_index do |token, i|
+            if token == 'factory'
+              factory = tokens[i + 3]
+              puts "\tfactory: #{factory}"
+            elsif token == 'trait'
+              trait = tokens[i + 3]
+              puts "\t\ttrait: #{trait}"
+            end
+          end
+        end
+      end
+    end
+  end
+
+  module_function :list
 end
